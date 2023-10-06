b3f179ab6a3c8d15c9e444978ea15ed1f69be0c8
marco polo <marcopolo081999@gmail.com>
2023-10-06 09:42:44 +0800

    update_print_out_noa_10/6/23

--- Fast_Report_API/Models/Noa.cs
+++ Fast_Report_API/Models/Noa.cs
@@ -15,7 +15,12 @@ namespace Fast_Report_API.Models
         //Company info from suppliers_tbl
         public string? supplier_name { get; set; }
         public string? contact_person { get; set; }
-
+        public string? delivery_address { get; set; }
+        public string? contact { get; set; }
+        public string? position { get; set; }
+        public string? fax_number { get; set; }
+        public string? pur_tbl { get; set; }
+        public string? attention_title { get; set; }
         //Approving authority
         public string? first_name { get; set; }
         public string? last_name { get; set; }
@@ -23,12 +28,23 @@ namespace Fast_Report_API.Models
         //Data from departments_tbl
         public string? department_name { get; set; }
 
+        //Data from mode_off_procurement
+        public string? mode_name { get; set; }
+        public string? mode_description { get; set; }
+
+        //Data for performance bond
+
+        public string? perf_sec_30 { get; set; }
+        public string? perf_sec_5 { get; set; }
+
+
         //Items info from noa_details_tbl
         //public string? description { get; set; }
         //public string? unit_cost { get; set; }
         //public string? total_cost { get; set; }
 
         public List<Noa_details>? noa_details { get; set; }
+        public List<Noa_terms>? terms_and_condition { get; set; }
 
     }
 
