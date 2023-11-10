31025a9d2b548942eaf2c58ffb7a6b9798f0263c
changco0987 <williamchangco26@gmail.com>
2023-11-10 10:51:14 +0800

    Created ApplicationForm_model and appForm frx file

---
+++ Fast_Report_API/Models/ApplicationForm_model.cs
@@ -0,0 +1,58 @@
+﻿namespace Fast_Report_API.Models
+{
+    public class ApplicationForm_model
+    {
+        public string? name { get; set; }
+        public string? age { get; set; }
+        public string? sex { get; set; }
+        public string? civil_status { get; set; }
+        public string? maiden_name { get; set; }
+        public string? spouse_name { get; set; }
+        public string? birth_date { get; set; }
+        public string? birth_place { get; set; }
+        public string? citizenship { get; set; }
+        public string? country_if_not_filipino { get; set; }
+        public string? address_city { get; set; }
+        public string? address_province { get; set; }
+        public string? telephone_no { get; set; }
+        public string? cellphone_no { get; set; }
+        public string? father_name { get; set; }
+        public string? mother_name { get; set; }
+        public string? position_applied { get; set; }
+        public string? specialty { get; set; }
+        public string? sub_specialty { get; set; }
+        public string? specialty_name { get; set; }
+        public List<Educational_background>? education { get; set; }
+        public List<Work_experience>? work_exp { get; set; }
+        public List<Recognitions>? recognitions { get; set; }
+        public List<References>? references { get; set; }
+
+    }
+
+    public class Educational_background 
+    {
+        public string? level { get; set; }
+        public string? degree { get; set; }
+        public string? school_name { get; set; }
+        public string? year_graduated { get; set; }
+    }
+
+    public class Work_experience
+    {
+        public string? type { get; set; }
+        public string? organization { get; set; }
+        public string? year_completed { get; set; }
+    }
+
+    public class Recognitions
+    {
+        public string? recognition_details { get; set; }
+    }
+
+    public class References
+    {
+        public string? name { get; set; }
+        public string? addres { get; set; }
+        public string? contact_no { get; set; }
+    }
+}
